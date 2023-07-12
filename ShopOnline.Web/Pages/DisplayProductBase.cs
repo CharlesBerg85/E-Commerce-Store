844f28f3e84a75329df72ef9f254301e7f657f8f
CharlesBerg85 <mr.charlesmberg@gmail.com>
2023-07-11 22:41:23 -0700

    ProductBase.cs

--- ShopOnline.Web/Pages/DisplayProductBase.cs
+++ ShopOnline.Web/Pages/DisplayProductBase.cs
@@ -1,6 +1,11 @@
-﻿namespace ShopOnline.Web.Pages
+﻿using Microsoft.AspNetCore.Components;
+using ShopOnline.Models.Dtos;
+
+namespace ShopOnline.Web.Pages
 {
-    public class DisplayProductBase
+    public class DisplayProductBase:ComponentBase
     {
+        [Parameter]
+        public IEnumerable<ProductDto>? Products { get; set; }
     }
 }
