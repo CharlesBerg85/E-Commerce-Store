23b92183b42bbe373b60bc21868f32f3ef4bdb5d
CharlesBerg85 <mr.charlesmberg@gmail.com>
2023-07-11 21:25:32 -0700

    Small Updates

--- ShopOnline.Web/Services/Contracts/IProductService.cs
+++ ShopOnline.Web/Services/Contracts/IProductService.cs
@@ -7,7 +7,7 @@ namespace ShopOnline.Web.Services.Contracts
         //We are going to wrap the functionality that handles calls to our web API component within services
         //Services are simply C# classes that wrap the functionality for handling interarion with our web API component
         //we want our method to run asynchronously so our method will return a Task object
-        //In this case we are going to write code to reurn a generic task object becaise this method must return a value to the calling code
+        //In this case we are going to write code to return a generic task object because this method must return a value to the calling code
         //The value type is an IEnumerable collection of objects of type productDto
         Task<IEnumerable<ProductDto>> GetItems();
     }
